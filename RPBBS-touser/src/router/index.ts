6f297fbe848f1b00449ad3d19eebaaa0061ca3a9
kzy zz <1152297595@qq.com>
2023-02-04 18:12:26 +0800

    仓库迁移

---
+++ RPBBS-touser/src/router/index.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Layout from '@/layout/index.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      component: Layout,
+      path: '/',
+      children: [
+
+      ]
+    }
+  ]
+})
+
+export default router
