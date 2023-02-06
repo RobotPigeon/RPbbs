a5117bd989b064f3d43aa0dec3bb94bea7e2f01b
kzy zz <1152297595@qq.com>
2023-02-06 20:41:50 +0800

    文件结构梳理

--- RPBBS-touser/src/layout/index.vue
+++ RPBBS-touser/src/layout/index.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="flex h-screen">
         <div class="flex-1 flex flex-col overflow-hidden">
-            <Header class="z-50"></Header>
+            <Header class="z-50 bg-gray-600"></Header>
             <router-view></router-view>
         </div>
     </div>
 </template>
     
 <script setup lang='ts'>
-import Header from './home/header/index.vue'
+import Header from '../view/home/header/index.vue'
 import { ref, reactive } from 'vue'
 </script>
     
--- RPBBS-touser/src/router/index.ts
+++ RPBBS-touser/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Layout from '@/layout/index.vue'
-import Home from '@/layout/home/index.vue'
+import Home from '@/view/home/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
