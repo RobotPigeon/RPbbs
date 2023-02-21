42195c7aaeab71c197305d6546fcdcb3b94264b4
kzy zz <1152297595@qq.com>
2023-02-22 01:24:57 +0800

    滚动到底部判断实现

--- RPBBS-touser/src/components/basic/rp-picgroup.vue
+++ RPBBS-touser/src/components/basic/rp-picgroup.vue
@@ -5,28 +5,22 @@
         :imgSrc="imgSrc"
         class="img-big"
     ></rpImg2big>
-    <div class="overflow-hidden h-40vh mx-6 w-40vh rounded-box relative flex flex-wrap" v-if='props.list.length == 3'>
-        <div class=" h-45% w-45% mx-1 bg-black rounded-box relative overflow-hidden my-1" v-for="item in props.list">
+    <!-- 图片小于等于三时 -->
+    <div class="overflow-hidden h-12vh mx-6 w-40vh rounded-box relative flex " v-if='props.list.length <= 3'>
+        <div class=" h-full w-30% mx-1 bg-black rounded-box relative overflow-hidden my-1" v-for="item in props.list">
             <img  class="absolute top-0 bottom-0 m-a" :src="item" @click="bigScale(item)" alt="" >
         </div>
     </div>
-    <div class="overflow-hidden h-40vh mx-6 w-40vh rounded-box relative flex flex-wrap" v-if='props.list.length == 4'>
-        <div class=" h-45% w-45% m-a bg-black rounded-box relative overflow-hidden my-1" v-for="item in props.list">
-            <img  class="absolute top-0 bottom-0 m-a" :src="item" @click="bigScale(item)" alt="" >
-        </div>
-    </div>
-    <div class="overflow-hidden h-40vh mx-6 w-40vh rounded-box relative flex flex-wrap" v-if='props.list.length>4'>
-        <div class=" h-45% w-45% m-a bg-black rounded-box relative overflow-hidden my-1">
+    <!-- 图片大于三时 -->
+    <div class="overflow-hidden h-12vh mx-6 w-40vh rounded-box relative flex" v-if='props.list.length>3'>
+        <div class=" h-full w-30% mx-1 bg-black rounded-box relative overflow-hidden my-1">
             <img  class="absolute top-0 bottom-0 m-a" :src="props.list[0]" @click="bigScale(props.list[0])" alt="" >
         </div>
-        <div class=" h-45% w-45% m-a bg-black rounded-box relative overflow-hidden my-1">
+        <div class=" h-full w-30% mx-1 bg-black rounded-box relative overflow-hidden my-1">
             <img  class="absolute top-0 bottom-0 m-a" :src="props.list[1]" @click="bigScale(props.list[1])" alt="" >
         </div>
-        <div class=" h-45% w-45% m-a bg-black rounded-box relative overflow-hidden my-1">
-            <img  class="absolute top-0 bottom-0 m-a" :src="props.list[2]" @click="bigScale(props.list[2])" alt="" >
-        </div>
-        <div class=" h-45% w-45% m-a bg-black rounded-box relative overflow-hidden my-1">
-            <div class="title items-center sm:text-6xl xl:text-20 flex h-100% w-50% m-a cellphone">+{{props.list.length-3}}</div>
+        <div class=" h-full w-30% mx-1 bg-black rounded-box relative overflow-hidden my-1">
+            <div class="title items-center sm:text-6xl xl:text-6xl flex h-100% w-50% m-a cellphone">+{{props.list.length-2}}</div>
         </div>
     </div>
 </template>
@@ -63,9 +57,9 @@ const props = withDefaults(defineProps<Props>(), {
 <style lang="less">
 @media (max-width: 640px) {
 
-.cellphone {
-    font-size: 50px;
-}
+    .cellphone {
+        font-size: 30px;
+    }
 
 }
 </style>
--- RPBBS-touser/src/view/home/content/index.vue
+++ RPBBS-touser/src/view/home/content/index.vue
@@ -1,30 +1,59 @@
 <template>
-  <main class="flex flex-col w-full overflow-y-auto mb-18 mt-1 b-1 rounded-box" >
-        <div class="flex w-full mx-auto px-0 py-0">
-          <div class="flex flex-col w-full h-full">
-            <rpCard :username="username" :liked="true" :like-num="10000" :comment-num="10000" :richtext="text" :useravatar="useravatar" :rank="rank" :piclist="piclist" :title="title"></rpCard>
-            <rpCard :username="username" :liked="true" :like-num="10000" :comment-num="10000" :richtext="text" :useravatar="useravatar" :rank="rank" :piclist="piclist" :title="title"></rpCard>
-            <rpCard :username="username" :liked="true" :like-num="10000" :comment-num="10000" :richtext="text" :useravatar="useravatar" :rank="rank" :piclist="piclist" :title="title"></rpCard>
-            <rpCard :username="username" :liked="true" :like-num="10000" :comment-num="10000" :richtext="text" :useravatar="useravatar" :rank="rank" :piclist="piclist" :title="title"></rpCard>
-            <rpCard :username="username" :liked="true" :like-num="10000" :comment-num="10000" :richtext="text" :useravatar="useravatar" :rank="rank" :piclist="piclist" :title="title"></rpCard>
-          </div>
-        </div>
-        
-      </main>
+  <main class="flex flex-col w-full overflow-y-auto mb-18 mt-1 b-1 rounded-box content">
+    <div class="flex w-full mx-auto px-0 py-0">
+      <div class="flex flex-col w-full h-full">
+        <rpCard :username="username" :liked="true" :like-num="10000" :comment-num="10000" :richtext="text"
+          :useravatar="useravatar" :rank="rank" :piclist="piclist" :title="title"></rpCard>
+        <rpCard :username="username" :liked="true" :like-num="10000" :comment-num="10000" :richtext="text"
+          :useravatar="useravatar" :rank="rank" :piclist="piclist" :title="title"></rpCard>
+        <rpCard :username="username" :liked="true" :like-num="10000" :comment-num="10000" :richtext="text"
+          :useravatar="useravatar" :rank="rank" :piclist="piclist" :title="title"></rpCard>
+        <rpCard :username="username" :liked="true" :like-num="10000" :comment-num="10000" :richtext="text"
+          :useravatar="useravatar" :rank="rank" :piclist="piclist" :title="title"></rpCard>
+        <rpCard :username="username" :liked="true" :like-num="10000" :comment-num="10000" :richtext="text"
+          :useravatar="useravatar" :rank="rank" :piclist="piclist" :title="title"></rpCard>
+      </div>
+    </div>
 
+  </main>
 </template>
 
 <script setup lang='ts'>
-import { ref, reactive } from 'vue'
+import { ref, reactive, onMounted, onUnmounted } from 'vue'
+import type { Ref } from 'vue';
 import rpCard from '@/components/basic/rp-card.vue';
-const piclist: Array<string> = ['https://lain.bgm.tv/r/400/pic/cover/l/a4/16/296739_71dLe.jpg', 'https://lain.bgm.tv/pic/cover/l/2b/03/406604_iYYvi.jpg', 'https://lain.bgm.tv/pic/cover/l/64/f0/420030_R3z00.jpg', ]
+const loadflag: Ref<boolean> = ref(false);
+const piclist: Array<string> = ['https://lain.bgm.tv/r/400/pic/cover/l/a4/16/296739_71dLe.jpg', 'https://lain.bgm.tv/pic/cover/l/2b/03/406604_iYYvi.jpg', 'https://lain.bgm.tv/pic/cover/l/64/f0/420030_R3z00.jpg', 'https://lain.bgm.tv/pic/cover/l/64/f0/420030_R3z00.jpg']
 const username: string = '猪逼巴巴'
 const title: string = '如何评价首先是然后再是'
 const useravatar: string = 'https://lain.bgm.tv/r/400/pic/cover/l/a4/16/296739_71dLe.jpg'
 const rank: number = 99
 const text: string = '<p>啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊</p><p>啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊</p>'
+
+//监听滚动方法
+function scrollHandle() {
+  const content = document.getElementsByClassName('content')[0];
+  const scrollHeight = content.scrollHeight
+  const scrollTop = content.scrollTop
+  const clientHeight = content.clientHeight;
+  const distance = scrollHeight - scrollTop - clientHeight;
+  if (distance <= 200) {
+    console.log(distance);
+
+  }
+}
+
+onMounted(() => {
+  //组件挂载时，添加scroll监听
+  const content = document.getElementsByClassName('content')[0];
+  content.addEventListener("scroll", scrollHandle, true);
+});
+onUnmounted(() => {
+  //组件卸载时，停止监听
+  const content = document.getElementsByClassName('content')[0];
+  content.removeEventListener("scroll", scrollHandle, false);
+});
+
 </script>
 
-<style scoped lang='less'>
-
-</style>
+<style scoped lang='less'></style>
