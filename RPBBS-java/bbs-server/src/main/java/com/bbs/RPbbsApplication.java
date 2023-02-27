bfbe84a9bf0aa65a8cbba28e503bfb80fdb0ef2f
wengws <wengws@winsdom.com>
2023-02-27 18:06:38 +0800

    bbs模块-启动类

---
+++ RPBBS-java/bbs-server/src/main/java/com/bbs/RPbbsApplication.java
@@ -0,0 +1,13 @@
+package com.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RPbbsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RPbbsApplication.class, args);
+        System.out.println("========bbs模块开启！========");
+
+    }
+}
