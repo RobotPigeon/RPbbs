ecb78743171f0968993dba8f45baae84843556e7
wengws <wengws@winsdom.com>
2023-02-28 18:12:58 +0800

    帖子类型数据

---
+++ RPBBS-sql/card_type_insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO rpbbs.card_type
+(id, type_name, create_time, update_time, status)
+VALUES(1, '图文帖子', '2023-02-28 00:00:00', '2023-02-28 00:00:00', 0);
+INSERT INTO rpbbs.card_type
+(id, type_name, create_time, update_time, status)
+VALUES(2, '资源帖子', '2023-02-28 00:00:00', '2023-02-28 00:00:00', 0);
