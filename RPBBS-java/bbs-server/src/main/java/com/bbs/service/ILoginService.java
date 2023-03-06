2beeeb130863a7cc7d34de190b95c15ed78cc5c8
wengws <wengws@winsdom.com>
2023-03-06 17:34:34 +0800

    feat: bbs模块 - 注释添加

--- RPBBS-java/bbs-server/src/main/java/com/bbs/service/ILoginService.java
+++ RPBBS-java/bbs-server/src/main/java/com/bbs/service/ILoginService.java
@@ -3,6 +3,13 @@ package com.bbs.service;
 import com.bbs.domain.User;
 
 public interface ILoginService {
+
+    /**
+     * 账号密码验证成功，返回一段token，否则返回空字符串
+     * @param username
+     * @param password
+     * @return
+     */
     public String login(String username, String password);
 
 }
