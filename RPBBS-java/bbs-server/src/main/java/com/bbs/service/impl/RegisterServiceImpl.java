e0fcf78ec84efbff32e62778a68c625f250ec3cb
“wengws” <wengws@winsdom.com>
2023-04-02 15:56:24 +0800

    fix: 用户注册-用户信息初始化、token设定有效期  用户信息修改-sql修改

--- RPBBS-java/bbs-server/src/main/java/com/bbs/service/impl/RegisterServiceImpl.java
+++ RPBBS-java/bbs-server/src/main/java/com/bbs/service/impl/RegisterServiceImpl.java
@@ -1,40 +1,58 @@
 package com.bbs.service.impl;
 
 import com.bbs.domain.User;
-import com.bbs.mapper.UserMapper;
+import com.bbs.domain.UserInfo;
 import com.bbs.service.IRegisterService;
+import com.bbs.service.IUserInfoService;
+import com.bbs.service.IUserService;
 import com.bbs.utils.DateTimeUtils;
 import com.bbs.utils.MD5Utils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
+
 @Slf4j
 @Service
 public class RegisterServiceImpl implements IRegisterService {
 
     @Autowired
-    private UserMapper userMapper;
+    private IUserService userService;
+
+    @Autowired
+    private IUserInfoService userInfoService;
 
     @Override
     public int addUser(User message) {
         User user = new User();
         user.setUsername(message.getUsername());
         // 账号名查重
-        if (!userMapper.selectUserList(user).isEmpty()) {
-            return 1;
-        }else {
+        if (userService.selectUserList(user).isEmpty()) {
             user.setPassword(MD5Utils.getHash(message.getPassword()));
             user.setCreateTime(DateTimeUtils.now());
             user.setUpdateTime(user.getCreateTime());
             user.setStatus(0L);
             try {
-                userMapper.insertUser(user);
+                userService.insertUser(user);
             }catch (Exception e){
                 log.error(e.getMessage());
                 return -1;
             }
+
+            // 用户信息初始化
+            try {
+                User user1 = userService.selectUserList(user).get(0);
+                UserInfo userInfo = new UserInfo();
+                userInfo.setUserId(String.valueOf(user1.getId()));
+                userInfo.setLevel(1L);
+                userInfoService.insertUserInfo(userInfo);
+            }catch (Exception e) {
+                log.error(e.getMessage());
+            }
             return 0;
+        }else {
+            return 1;
         }
     }
 }
