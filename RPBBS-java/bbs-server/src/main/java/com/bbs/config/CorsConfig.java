c95b2765b204ece0f68274e45d0761785dd56225
wengws <wengws@winsdom.com>
2023-04-04 14:48:45 +0800

    fix: CorsConfig修改

--- RPBBS-java/bbs-server/src/main/java/com/bbs/config/CorsConfig.java
+++ RPBBS-java/bbs-server/src/main/java/com/bbs/config/CorsConfig.java
@@ -9,18 +9,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * 跨域请求配置
  */
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowCredentials(true)
-                        .allowedMethods("*")
-                        .allowedOrigins("http://127.0.0.1:5173");
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5173")
+                .allowedHeaders("*")
+                .allowedMethods("*");
     }
 
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowCredentials(true)
+//                        .allowedMethods("*")
+//                        .allowedOrigins("http://127.0.0.1:5173");
+//            }
+//        };
+//    }
 }
