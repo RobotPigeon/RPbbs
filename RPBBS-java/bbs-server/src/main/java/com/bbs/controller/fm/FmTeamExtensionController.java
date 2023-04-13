a120c5965e7200e6478ab8cdad4182c4e8d536bc
“wengws” <wengws@winsdom.com>
2023-04-13 16:48:56 +0800

    feat: 以id获取单个TeamVo接口

--- RPBBS-java/bbs-server/src/main/java/com/bbs/controller/fm/FmTeamExtensionController.java
+++ RPBBS-java/bbs-server/src/main/java/com/bbs/controller/fm/FmTeamExtensionController.java
@@ -3,6 +3,7 @@ package com.bbs.controller.fm;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.bbs.domain.msg.AjaxResult;
+import com.bbs.domain.vo.FmTeamVo;
 import com.bbs.service.IFmTeamExtensionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +24,15 @@ public class FmTeamExtensionController {
             return AjaxResult.error("未获得数据");
         }
         return AjaxResult.success(list);
+    }
+
+    @GetMapping("/{id}")
+    public AjaxResult get(@PathVariable("id") Long id) {
+        FmTeamVo data = fmTeamExtensionService.selectFmPlayerVoById(id);
+        if (data == null) {
+            return AjaxResult.error("没有获取到数据");
+        }
+        return AjaxResult.success(data);
     }
 
     @GetMapping("/list")
